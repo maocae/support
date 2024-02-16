a402c5d1781f52fb404628cfde0220a8b7183184
Ritsu Hezayuki <ritsu.hezayuki@gmail.com>
2024-02-16 11:01:07 +0700

    Add CallStatically trait

--- src/Patterns/Singleton.php
+++ src/Patterns/Singleton.php
@@ -8,4 +8,15 @@ use Maocae\Support\Patterns\Traits\CallOnce;
 class Singleton implements MustCallOnce
 {
     use CallOnce;
+
+    /**
+     * Check if given class or object is implementing singleton
+     *
+     * @param object|string $class
+     * @return bool
+     */
+    public static function isSingleton(object|string $class): bool
+    {
+        return is_a($class, MustCallOnce::class, is_string($class));
+    }
 }
